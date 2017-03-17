4646703fcbdcf188caf77e07d15abf7be1d17fa4
Marcel Neumair <neumairmarcel@gmail.com>
2017-03-17 14:17:19 +0100

    Working Project

--- src/Faculty.java
+++ src/Faculty.java
@@ -3,11 +3,6 @@
  */
 public class Faculty {
 
-    public static void main(String args[]){
-        Faculty sick = new Faculty();
-        System.out.print(sick.faculty(5));
-    }
-
     public int faculty(int fac){
         if (fac == 0)
             return 1;
